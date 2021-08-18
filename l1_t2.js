70e11c30b12837953f9a7d989ddd3e37d8d09f79
Илья Марчуков <Gerald16311@gmail.com>
2021-08-18 16:16:40 +0400

    all done

--- l1_t2.js
+++
@@ -1,7 +0,0 @@
-/*Объявить две переменные: admin и name.Записать в name строку "Василий"; Скопировать значение из name в admin.Вывести admin(должно вывестись «Василий»).*/
-var admin;
-var name;
-
-name = "Василий";
-admin = name;
-alert(admin)
