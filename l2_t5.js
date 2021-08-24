bf603025945539373f9f707da83f9c432dd060bc
Илья Марчуков <Gerald16311@gmail.com>
2021-08-24 10:16:17 +0400

    All done

---
+++ l2_t5.js
@@ -0,0 +1,24 @@
+/* ### 5. Реализовать основные 4 арифметические операции 
+    в виде функций с двумя параметрами. Обязательно использовать оператор return.
+*/
+function addition(a,b){
+    return (a + b)
+}
+function subtraction(a,b){
+    return (a - b)
+}
+
+function multiplication(a,b){
+    return (a * b)
+}
+
+function division(a,b){
+    return (a / b)
+}
+
+let a = 2, b = 2;
+
+console.log (addition(a, b));
+console.log (subtraction(a, b));
+console.log (multiplication(a, b));
+console.log (division(a, b));
