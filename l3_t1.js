2bcb55683f0b0244a6951de85971a874ac627303
Илья Марчуков <Gerald16311@gmail.com>
2021-08-25 09:10:25 +0400

    rebuild

--- l3_t1.js
+++ l3_t1.js
@@ -1,7 +1,13 @@
 /*1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.*/
-let i = 0;
-
-while (i<101){
-    console.log (i)
-    i++
+for (let i = 2;i<=100;i++){
+    isPrime = true
+    for(let y = 2; y<i; y++){
+        if (i%y ===0){
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime){
+        console.log(i)
+    }
 }
