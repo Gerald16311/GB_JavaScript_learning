f553ac79c7e9e954d1fc0de85f81f65bf4b77db3
Илья Марчуков <Gerald16311@gmail.com>
2021-08-27 11:14:36 +0400

    all done

--- l4_t2.js
+++ l4_t2.js
@@ -1,1 +1,46 @@
-/*2. Для игры, реализованной на уроке, добавить возможность вывода хода номер n (номер задается пользователем) */
+/*Продолжить работу с интернет-магазином:
+В прошлом домашнем задании вы реализовали корзину на базе массивов. Какими объектами можно заменить их 
+элементы?
+Реализуйте такие объекты.
+Перенести функционал подсчета корзины на объектно-ориентированную базу.
+
+ Код из предидущего задания*/
+
+class products{
+    constructor(name, price, count){
+        this.name = name;
+        this.price = price;
+    }
+}
+
+let product1 = new products("Картошка", 50);
+let product2 = new products("Огурцы", 20);
+let product3 = new products("Помидоры", 10);
+let product4 = new products("Кабачки", 43);
+let product5 = new products("Баклажаны", 32);
+let product6 = new products("Морковка", 60);
+let product7 = new products("Свекла", 35);
+
+
+function countBasketPrice(basket){
+    let count = 0;
+    for (item in basket){
+        count += basket[item].price;
+    }
+    return count
+}
+
+var userBasket = [];
+
+userBasket.push(product1);
+userBasket.push(product1);
+userBasket.push(product2);
+userBasket.push(product3);
+userBasket.push(product4);
+userBasket.push(product5);
+userBasket.push(product6);
+userBasket.push(product7);
+
+console.log(countBasketPrice(userBasket))
+
+console.log(userBasket)
